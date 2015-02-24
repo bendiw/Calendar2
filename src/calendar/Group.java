8d35148f4e92542a2199873d9d8e6410ad2a63cf
Bendik Witzoe <bendikw@gmail.com>
2015-02-24 10:58:30 +0100

    Nå da

--- src/calendar/Group.java
+++ src/calendar/Group.java
@@ -6,7 +6,7 @@ public class Group {
 	
 	private ArrayList<Person> members;
 	private Calendar cal;
-	
+	private String name;
 	
 
 }
--- src/calendar/KalenderTest.java
+++ src/calendar/KalenderTest.java
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-package calendar;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-
-public class KalenderTest {
-	
-	private String he;
-	}
-}
-=======
 package calendar;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -19,6 +7,5 @@ public class KalenderTest {
 	
 	
 	public static void main(String[] args) {
-		String c = "halla på deg din nisse";
+		String c = "s";
 }
->>>>>>> branch 'master' of https://github.com/bendiw/calendar.git
