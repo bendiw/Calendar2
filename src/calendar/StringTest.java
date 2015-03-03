943c3351f2da630ce795dddbb1bade5d0ae7b22b
rakelhakegard <rakelhakegard@dhcp-049036.wlan.ntnu.no>
2015-03-03 11:45:52 +0100

    Jeg har endret litt :))

--- src/calendar/StringTest.java
+++ src/calendar/StringTest.java
@@ -2,26 +2,8 @@ package calendar;
 
 public class StringTest {
 
-	boolean statement;
 	
-	public boolean hasNext(){
-		if(statement){
-			if(row=)
-				
-				
-				
-				return 
-		}else{
-			
-			
-			
-			
-		}
-	}
-	
-	[i][j]
-			
-		
+
 	
 	
 	
