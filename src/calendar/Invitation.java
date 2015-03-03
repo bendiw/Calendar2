4c69bc9d9ec32ca0d608f31b29eebf551750c125
rakelhakegard <rakelhakegard@dhcp-049036.wlan.ntnu.no>
2015-03-03 12:24:24 +0100

    Har gjort endringer i Inv

--- src/calendar/Invitation.java
+++ src/calendar/Invitation.java
@@ -4,10 +4,27 @@ import java.util.List;
 
 public class Invitation {
 
+	private Meeting meeting;
+	private List<Person> invitedPersons;
+	private List<Group> invitedGroups;
 	
-	List<Person> invitedPersons;
-	List<Group> invitedGroups;
-	
+	private void sendInvite(Group g) {
+			
+	}
+		
+	private void sendInvite(Person p) {
+			
+	}
+		
+	protected void respond(Person p, boolean ans) {
+		if (ans == true) {
+			// legg til p i attendinglisten til meeting
+		}
+		else {
+			// varsle fra om at p har avslått?
+		}
+	}
+		
 	
 	
 }
