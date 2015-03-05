8115f1514604419aefe97c6540e7d4ac67739bd6
LarsHenrik <larshbj@gmail.com>
2015-03-05 12:22:58 +0100

    personbuilder

---
+++ src/calendar/PersonBuilder.java
@@ -0,0 +1,21 @@
+package src.calendar;
+
+public class PersonBuilder {
+
+
+	
+	public List<Person> getAllPersons(){
+		List<Person> toReturn = new ArrayList<Person>();
+		statement sapdspoa
+		for (String name : fetchedNames) {
+			Person toAdd = new Person(name, email, IDno);
+			toReturn.add(toAdd);
+		}
+		return toReturn;
+	}
+	
+	public Person getPerson(int id){
+		
+	}
+	
+}
